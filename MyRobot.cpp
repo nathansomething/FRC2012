01280eee778106d99057776904be70b56a9291ec
Nathan Goodman <Nathan.something@gmail.com>
2012-01-21 10:40:41 -0800

    removed watchdog code

--- MyRobot.cpp
+++ MyRobot.cpp
@@ -12,18 +12,16 @@ const double WATCHDOG_EXPIRATION = 0.1;
 
 class RobotDemo : public SimpleRobot
 {
-	//RobotDrive *myRobot; // robot drive system
 	Jaguar *LeftDriveJaguar;
 	Jaguar *RightDriveJaguar;
-	Watchdog *Saftey;
-	Joystick *LeftDriveJoystick;
-	Joystick *RightDriveJoystick;
-	DriverStationLCD *dsLCD;
-	DriverStation *ds;
-	DriverStationEnhancedIO *dsIO;
+	Joystick *LeftJoystickDrive;
+	Joystick *RightJoystickDrive;
+	//DriverStationLCD *dsLCD;
+	//DriverStation *ds;
+	//DriverStationEnhancedIO *dsIO;
 	AxisCamera *Camera;
-	
-	
+
+
 	//Joystick stick; // only joystick
 
 public:
@@ -31,32 +29,20 @@ public:
 		//myRobot(6, 7)	// these must be initialized in the same order
 				// as they are declared above.
 	{
-		//myRobot = new RobotDrive(6,7);
 		LeftDriveJaguar = new Jaguar(6);
 		RightDriveJaguar = new Jaguar(7);
-		Saftey = new Watchdog;
-		LeftDriveJoystick = new Joystick(1);
-		RightDriveJoystick = new Joystick(2);
-		//dsIO = new DriverStationEnhancedIO;
-		//dsLCD = new DriverStationLCD;
-		//myRobot->SetExpiration(0.1);
+		LeftJoystickDrive = new Joystick(1);
+		RightJoystickDrive = new Joystick(2);
 	}
 	/**
 	 * Drive left & right motors for 2 seconds then stop
 	 */
 	void Autonomous(void)
 	{
-		Saftey->SetEnabled(false);
-		//myRobot->SetSafetyEnabled(false);
-		//myRobot->Drive(0.5, 0.0); 	// drive forwards half speed
-		dsLCD = DriverStationLCD::GetInstance();
-		dsLCD->Clear();
-		//dsLCD->Printf(DriverStationLCD::kUser_Line1, 1, "Hello World" );
-		//dsLCD->UpdateLCD();
-		AutonomousDrive(1.0,0.0);
-		Wait(0.5);
-		
-		ds = DriverStation::GetInstance();
+		//dsLCD = DriverStationLCD::GetInstance();
+		//dsLCD->Clear();
+
+		/*ds = DriverStation::GetInstance();
 		switch(ds->GetLocation())
 		{
 		case 1:
@@ -71,85 +57,88 @@ public:
 			dsLCD->Printf(DriverStationLCD::kUser_Line1, 1, "Executing Autonomous 3");
 			//Execute Autonomous code #3
 			break;
-			
-		}
-		dsLCD->UpdateLCD();
-		
-		Saftey->SetEnabled(false);
-		Wait(0.5); 				//    for 2 seconds
+		}*/
+		//dsLCD->UpdateLCD();
+		DriveAutonomously(1.0, 0.0);
+		Wait(1); 		
 		delete LeftDriveJaguar;
 		delete RightDriveJaguar;
-		delete Saftey;
-		delete dsLCD;
-		delete ds;
+		delete LeftJoystickDrive;
+		delete RightJoystickDrive;
+		//delete dsLCD;
+		//delete ds;
 	}
 
+	/**
+	 * Runs the motors with arcade steering. 
+	 */
 	void OperatorControl(void)
 	{
 		HSLImage *Himage;
 		Threshold targetThreshold(247, 255, 60, 140, 10, 50);
 		BinaryImage *matchingPixels;
 		vector<ParticleAnalysisReport> *pReport;
-		
-		Saftey->SetEnabled(true);
+
 		AxisCamera &mycam = AxisCamera::GetInstance("10.15.10.11");
-		
+
 		mycam.WriteResolution(AxisCamera::kResolution_640x480);
 		mycam.WriteCompression(20);
 		mycam.WriteBrightness(25);
-		
-		dsLCD = DriverStationLCD::GetInstance();
-		dsLCD->Clear();
-		
+         
+		//dsLCD = DriverStationLCD::GetInstance();
+		//dsLCD->Clear();
+
 		while(IsOperatorControl())
 		{
-			Saftey->Feed();
-			UpdateStickDrive();
-			Wait(0.005);
+			UpdateJoystickDrive();
 			if (mycam.IsFreshImage())
+			{
+				Himage = mycam.GetImage();
+
+				matchingPixels = Himage->ThresholdHSL(targetThreshold);
+				pReport = matchingPixels->GetOrderedParticleAnalysisReports();
+
+				for (unsigned int i = 0; i < pReport->size(); i++)
 				{
-					Himage = mycam.GetImage();
-					
-					matchingPixels = Himage->ThresholdHSL(targetThreshold);
-					pReport = matchingPixels->GetOrderedParticleAnalysisReports();
-					
-					for (unsigned int i = 0; i < pReport->size(); i++)
-					{
-						printf("Index: %d X Center: %d Y Center: %d \n", i, (*pReport)[i].center_mass_x, (*pReport)[i].center_mass_y);
-					}
-					
-					delete Himage;
-					delete matchingPixels;
-					delete pReport;
+					printf("Index: %d X Center: %d Y Center: %d \n", i, (*pReport)[i].center_mass_x, (*pReport)[i].center_mass_y);
 				}
+			}
+			Wait(0.005);
 		}	
+		delete Himage;
+		delete matchingPixels;
+		delete pReport;
 	}
 	
-	void UpdateStickDrive() //Gets the value of the left and right joystick and sets it to the power of the left and right jaguars
-	{	
-		LeftDriveJaguar->Set(LeftDriveJoystick->GetY());
-		RightDriveJaguar->Set(-RightDriveJoystick->GetY());
-	}
-	
-	void AutonomousDrive(float Speed, float Curve)
+	void DriveAutonomously(float Speed, float Curve)
 	{
-		if (Curve > 0) //turn right
+		if(Curve < 0) //Right Turn
 		{
 			LeftDriveJaguar->Set(Speed);
-			RightDriveJaguar->Set(-(Speed - (Speed * Curve)));
+			RightDriveJaguar->Set(Speed - (Curve * Speed));
 		}
-		if (Curve < 0) //turn left
+		if(Curve < 0) //Left Turn
 		{
-			LeftDriveJaguar->Set(Speed - (Speed * Curve));
-			RightDriveJaguar->Set(-Speed);
+			LeftDriveJaguar->Set(Speed - (Curve * Speed));
+			RightDriveJaguar->Set(Speed);
+		}
+		if(Curve == 0) //Straight
+		{
+			LeftDriveJaguar->Set(Speed);
+			RightDriveJaguar->Set(Speed);
 		}
 	}
+	void UpdateJoystickDrive() //Gets the value of the left and right joystick and sets it to the power of the left and right jaguars
+	{	
+		LeftDriveJaguar->Set(LeftJoystickDrive->GetY());
+		RightDriveJaguar->Set(RightJoystickDrive->GetY());
+	}
 	
-	void UpdateCamera()
+	~RobotDemo()
 	{
-		
+		delete LeftDriveJaguar;
+		delete RightDriveJaguar;
 	}
 };
 
 START_ROBOT_CLASS(RobotDemo);
-
